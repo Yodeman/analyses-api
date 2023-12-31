a7ca6329222477026a0f75116afa0b4737fb5f41
Paul Oyelabi <oyelabipaul@gmail.com>
2023-12-31 16:19:44 +0100

    [fix]: Corrected database definition.
        - Changed created_at dbase column default value.
        - Changed files 'data' column type.

--- dbase/migration/000001_init.up.sql
+++ dbase/migration/000001_init.up.sql
@@ -2,17 +2,17 @@ CREATE TABLE "users" (
   "username" varchar PRIMARY KEY,
   "hashed_password" varchar NOT NULL,
   "email" varchar UNIQUE NOT NULL,
-  "password_changed_at" timestamptz NOT NULL DEFAULT "0001-01-01 00:00:00+00",
+  "password_changed_at" timestamptz NOT NULL DEFAULT '0001-01-01 00:00:00+00',
   "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
 CREATE TABLE "files" (
   "id" bigserial PRIMARY KEY,
   "username" varchar NOT NULL,
-  "data" byte[],
+  "data" "char"[],
   "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
-CREATE UNIQUE INDEX ON "files" ("user");
+CREATE UNIQUE INDEX ON "files" ("username");
 
 ALTER TABLE "files" ADD FOREIGN KEY ("username") REFERENCES "users" ("username");
