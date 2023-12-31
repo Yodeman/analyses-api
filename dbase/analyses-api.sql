9c8396eb03c8db22d758243989695abdf7113d3d
Paul Oyelabi <oyelabipaul@gmail.com>
2023-12-31 15:07:50 +0100

    [update]: Added database definition.

---
+++ dbase/analyses-api.sql
@@ -0,0 +1,18 @@
+CREATE TABLE "users" (
+  "username" varchar PRIMARY KEY,
+  "hashed_password" varchar NOT NULL,
+  "email" varchar UNIQUE NOT NULL,
+  "password_changed_at" timestamptz NOT NULL DEFAULT (0001-01-01 00:00:00Z),
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "files" (
+  "id" bigserial PRIMARY KEY,
+  "username" varchar NOT NULL,
+  "data" byte[],
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE UNIQUE INDEX ON "files" ("user");
+
+ALTER TABLE "files" ADD FOREIGN KEY ("username") REFERENCES "users" ("username");
