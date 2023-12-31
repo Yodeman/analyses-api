f9af79eef6ddab9f057862eb54e516fe52c45071
Paul Oyelabi <oyelabipaul@gmail.com>
2023-12-31 20:13:07 +0100

    [update]: Added test for database GetUser query.

--- dbase/sqlc/dbtest/users_test.go
+++ dbase/sqlc/dbtest/users_test.go
@@ -85,6 +85,94 @@ func TestCreateUser(t *testing.T) {
     }
 }
 
+func TestGetUser(t *testing.T) {
+    user, password := randomUser(t)
+    var ctx context.Context
+
+    testCases := []struct{
+        name        string
+        param       string
+        buildStubs  func(querier *mockdb.MockQuerier)
+        checkResult func(t *testing.T, result db.User, err error)
+    }{
+        {
+            name:       "OK",
+            param:      user.Username,
+            buildStubs: func(querier *mockdb.MockQuerier) {
+                querier.EXPECT().
+                    GetUser(gomock.Any(), gomock.Eq(user.Username)).
+                    Times(1).
+                    Return(user, nil)
+            },
+            checkResult:    func(t *testing.T, result db.User, err error) {
+                require.NoError(t, err)
+                require.NotEmpty(t, result)
+                require.Equal(t, user.Username, result.Username)
+                require.Equal(t, user.Email, result.Email)
+                require.WithinDuration(t, user.CreatedAt, result.CreatedAt, time.Second)
+                require.NoError(t, util.CheckPassword(password, result.HashedPassword))
+            },
+        },
+        {
+            name:       "NOT FOUND",
+            param:      "",
+            buildStubs: func(querier *mockdb.MockQuerier) {
+
+                querier.EXPECT().
+                    GetUser(gomock.Any(), gomock.Any()).
+                    Times(1).
+                    Return(db.User{}, sql.ErrNoRows)
+            },
+            checkResult:    func(t *testing.T, result db.User, err error) {
+                require.Error(t, err)
+                require.Empty(t, result)
+                require.Zero(t, result.Username)
+                require.Zero(t, result.Email)
+                require.Zero(t, result.CreatedAt)
+                require.Error(t, util.CheckPassword(password, result.HashedPassword))
+            },
+        },
+        {
+            name:       "INTERNAL ERROR",
+            param:      user.Username,
+            buildStubs: func(querier *mockdb.MockQuerier) {
+
+                querier.EXPECT().
+                    GetUser(gomock.Any(), gomock.Eq(user.Username)).
+                    Times(1).
+                    Return(db.User{}, sql.ErrConnDone)
+            },
+            checkResult:    func(t *testing.T, result db.User, err error) {
+                require.Error(t, err)
+                require.Empty(t, result)
+                require.Zero(t, result.Username)
+                require.Zero(t, result.Email)
+                require.Zero(t, result.CreatedAt)
+                require.Error(t, util.CheckPassword(password, result.HashedPassword))
+            },
+        },
+    }
+
+    for i := range testCases {
+        tc := testCases[i]
+
+        t.Run(tc.name, func(t *testing.T){
+            ctrl := gomock.NewController(t)
+            defer ctrl.Finish()
+
+            testQuerier := mockdb.NewMockQuerier(ctrl)
+
+            //build stubs
+            tc.buildStubs(testQuerier)
+
+            result, err := testQuerier.GetUser(ctx, tc.param)
+
+            tc.checkResult(t, result, err)
+        })
+    }
+}
+
+
 func randomUser(t *testing.T) (user db.User, password string) {
     password = util.RandomPassword()
     hashedPassword, err := util.HashPassword(password)
