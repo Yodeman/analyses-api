f8ae4ebaa20f76a6785d5e2474d2c61bd4a000a3
Paul Oyelabi <oyelabipaul@gmail.com>
2024-01-01 14:35:10 +0100

    added test for get and update queries on files

--- dbase/sqlc/dbtest/files_test.go
+++ dbase/sqlc/dbtest/files_test.go
@@ -89,3 +89,198 @@ func TestCreateFile(t *testing.T) {
         })
     }
 }
+
+func TestGetFile(t *testing.T) {
+    user, _ := randomUser(t)
+    data, err := util.RandomData()
+    require.NoError(t, err)
+
+    file := db.File{
+        Username:   user.Username,
+        Data:       string(data),
+    }
+
+    var ctx context.Context
+
+    testCases := []struct{
+        name        string
+        param       string
+        buildStubs  func(querier *mockdb.MockQuerier)
+        checkResult func(t *testing.T, result db.File, err error)
+    }{
+        {
+            name:       "OK",
+            param:      user.Username,
+            buildStubs: func(querier *mockdb.MockQuerier) {
+                querier.EXPECT().
+                    GetFile(gomock.Any(), gomock.Eq(user.Username)).
+                    Times(1).
+                    Return(file, nil)
+            },
+            checkResult:    func(t *testing.T, result db.File, err error) {
+                require.NoError(t, err)
+                require.NotEmpty(t, result)
+                require.Equal(t, file.Username, result.Username)
+                require.Equal(t, file.Data, result.Data)
+                require.WithinDuration(t, user.CreatedAt, result.CreatedAt, time.Second)
+            },
+        },
+        {
+            name:       "INTERNAL ERROR",
+            param:      user.Username,
+            buildStubs: func(querier *mockdb.MockQuerier) {
+
+                querier.EXPECT().
+                    GetFile(gomock.Any(), gomock.Eq(user.Username)).
+                    Times(1).
+                    Return(db.File{}, sql.ErrConnDone)
+            },
+            checkResult:    func(t *testing.T, result db.File, err error) {
+                require.Error(t, err)
+                require.Empty(t, result)
+                require.Zero(t, result.Username)
+                require.Zero(t, result.Data)
+                require.Zero(t, result.CreatedAt)
+            },
+        },
+        {
+            name:       "NOT FOUND",
+            param:      "",
+            buildStubs: func(querier *mockdb.MockQuerier) {
+
+                querier.EXPECT().
+                    GetFile(gomock.Any(), gomock.Any()).
+                    Times(1).
+                    Return(db.File{}, sql.ErrNoRows)
+            },
+            checkResult:    func(t *testing.T, result db.File, err error) {
+                require.Error(t, err)
+                require.Empty(t, result)
+                require.Zero(t, result.Username)
+                require.Zero(t, result.Data)
+                require.Zero(t, result.CreatedAt)
+            },
+        },
+    }
+
+    for i := range testCases {
+        tc := testCases[i]
+
+        t.Run(tc.name, func(t *testing.T){
+            ctrl := gomock.NewController(t)
+            defer ctrl.Finish()
+
+            testQuerier := mockdb.NewMockQuerier(ctrl)
+
+            //build stubs
+            tc.buildStubs(testQuerier)
+
+            result, err := testQuerier.GetFile(ctx, tc.param)
+
+            tc.checkResult(t, result, err)
+        })
+    }
+}
+
+func TestUpdateFile(t *testing.T) {
+    user, _ := randomUser(t)
+    data, err := util.RandomData()
+    require.NoError(t, err)
+
+    updateFileParams := db.UpdateFileParams{
+        Username:   user.Username,
+        Data:       string(data),
+    }
+
+    file := db.File{
+        Username:   user.Username,
+        Data:       string(data),
+    }
+
+    var ctx context.Context
+
+    testCases := []struct{
+        name        string
+        param       db.UpdateFileParams
+        buildStubs  func(querier *mockdb.MockQuerier)
+        checkResult func(t *testing.T, result db.File, err error)
+    }{
+        {
+            name:       "OK",
+            param:      updateFileParams,
+            buildStubs: func(querier *mockdb.MockQuerier) {
+                querier.EXPECT().
+                    UpdateFile(gomock.Any(), gomock.Eq(updateFileParams)).
+                    Times(1).
+                    Return(file, nil)
+            },
+            checkResult:    func(t *testing.T, result db.File, err error) {
+                require.NoError(t, err)
+                require.NotEmpty(t, result)
+                require.Equal(t, file.Username, result.Username)
+                require.Equal(t, file.Data, result.Data)
+                require.WithinDuration(t, user.CreatedAt, result.CreatedAt, time.Second)
+            },
+        },
+        {
+            name:       "INTERNAL ERROR",
+            param:      updateFileParams,
+            buildStubs: func(querier *mockdb.MockQuerier) {
+
+                querier.EXPECT().
+                    UpdateFile(gomock.Any(), gomock.Eq(updateFileParams)).
+                    Times(1).
+                    Return(db.File{}, sql.ErrConnDone)
+            },
+            checkResult:    func(t *testing.T, result db.File, err error) {
+                require.Error(t, err)
+                require.Empty(t, result)
+                require.Zero(t, result.Username)
+                require.Zero(t, result.Data)
+                require.Zero(t, result.CreatedAt)
+            },
+        },
+        {
+            name:       "NOT FOUND",
+            param:      db.UpdateFileParams{
+                Username:   "",
+                Data:       string(data),
+            },
+            buildStubs: func(querier *mockdb.MockQuerier) {
+                param := db.UpdateFileParams{
+                    Username:   "",
+                    Data:       string(data),
+                }
+                querier.EXPECT().
+                    UpdateFile(gomock.Any(), gomock.Eq(param)).
+                    Times(1).
+                    Return(db.File{}, sql.ErrNoRows)
+            },
+            checkResult:    func(t *testing.T, result db.File, err error) {
+                require.Error(t, err)
+                require.Empty(t, result)
+                require.Zero(t, result.Username)
+                require.Zero(t, result.Data)
+                require.Zero(t, result.CreatedAt)
+            },
+        },
+    }
+
+    for i := range testCases {
+        tc := testCases[i]
+
+        t.Run(tc.name, func(t *testing.T){
+            ctrl := gomock.NewController(t)
+            defer ctrl.Finish()
+
+            testQuerier := mockdb.NewMockQuerier(ctrl)
+
+            //build stubs
+            tc.buildStubs(testQuerier)
+
+            result, err := testQuerier.UpdateFile(ctx, tc.param)
+
+            tc.checkResult(t, result, err)
+        })
+    }
+}
