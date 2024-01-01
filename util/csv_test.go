901119258bfb80ab07f30cb6721511fb75154cf9
Paul Oyelabi <oyelabipaul@gmail.com>
2024-01-01 14:13:15 +0100

    added test for new random csv generator.

--- util/csv_test.go
+++ util/csv_test.go
@@ -8,14 +8,15 @@ import (
 )
 
 func TestParseCSVToFloatSlice(t *testing.T) {
-    sample_text := "1,2,3,4,5,6\n"+"7,8,9,10,11,12\n"+"13,14,15,16,17,18\n"
+    // sample_text := "1,2,3,4,5,6\n"+"7,8,9,10,11,12\n"+"13,14,15,16,17,18\n"
+    r, c := 10, 10
+    sample_text := RandomCSV(r, c)
 
-    r := strings.NewReader(sample_text)
+    reader := strings.NewReader(sample_text)
 
-    rows, cols, data, err := ParseCSVToFloatSlice(r)
+    rows, cols, data, err := ParseCSVToFloatSlice(reader)
     require.NoError(t, err)
-    require.Equal(t, 3, rows)
-    require.Equal(t, 6, cols)
+    require.Equal(t, r, rows)
+    require.Equal(t, c, cols)
     require.Equal(t, len(data), rows*cols)
-    require.Equal(t, []float64{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18}, data)
 }
