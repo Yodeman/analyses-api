6b7fb60685e5d6de50661bbdd1c983a228608bec
Paul Oyelabi <oyelabipaul@gmail.com>
2024-01-03 13:47:49 +0100

    added implementation for authorizatioin middleware
        -   the middleware ensure that request that makes changes to server
            carries authorization token.
        -   the middleware also verifies the access token carried by the
            request.

---
+++ api/middleware.go
@@ -0,0 +1,55 @@
+package api
+
+import (
+    "fmt"
+    "errors"
+    "net/http"
+    "strings"
+
+    "github.com/gin-gonic/gin"
+
+    "github.com/yodeman/analyses-api/token"
+)
+
+
+const (
+    authorizationHeaderKey      = "authorization"
+    authorizationPayloadKey     = "authorization_payload"
+
+    authorizationTypeToken      = "bearer"
+)
+
+func authMiddleware(tokenMaker *token.PasetoMaker) gin.HandlerFunc {
+    return func(ctx *gin.Context) {
+        authorizationHeader := ctx.GetHeader(authorizationHeaderKey)
+        if len(authorizationHeader) == 0 {
+            err := errors.New("Authorization header is not provided")
+            ctx.AbortWithStatusJSON(http.StatusUnauthorized, errResponse(err))
+            return
+        }
+
+        fields := strings.Fields(authorizationHeader)
+        if len(fields) < 2 {
+            err := errors.New("Invalid authorization header!")
+            ctx.AbortWithStatusJSON(http.StatusUnauthorized, errResponse(err))
+            return
+        }
+
+        authorizationType := strings.ToLower(fields[0])
+        switch authorizationType {
+            case authorizationTypeToken:
+                accessToken := fields[1]
+                payload, err := tokenMaker.VerifyToken(accessToken)
+                if err != nil {
+                    ctx.AbortWithStatusJSON(http.StatusUnauthorized, errResponse(err))
+                    return
+                }
+                ctx.Set(authorizationPayloadKey, payload)
+                ctx.Next()
+            default:
+                err := fmt.Errorf("Unsupported authorization type %s", authorizationType)
+                ctx.AbortWithStatusJSON(http.StatusUnauthorized, errResponse(err))
+        }
+    }
+
+}
