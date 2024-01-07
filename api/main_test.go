8cb22aa0fe70d22a2993660161b36b55aa04ad04
Paul Oyelabi <oyelabipaul@gmail.com>
2024-01-07 16:21:33 +0100

    added api test entry point

---
+++ api/main_test.go
@@ -0,0 +1,30 @@
+package api
+
+import (
+	"os"
+	"testing"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/require"
+
+	db "github.com/yodeman/analyses-api/dbase/sqlc"
+	"github.com/yodeman/analyses-api/util"
+)
+
+func newTestServer(t *testing.T, querier db.Querier) *Server {
+	config := util.Config{
+		TokenSymmetricKey:   util.RandomString(32),
+		AccessTokenDuration: time.Minute,
+	}
+
+	server, err := NewServer(config, querier)
+	require.NoError(t, err)
+
+	return server
+}
+
+func TestMain(m *testing.M) {
+	gin.SetMode(gin.TestMode)
+	os.Exit(m.Run())
+}
