59af59361f898f44a88108ead4881cb2e386c030
Paul Oyelabi <oyelabipaul@gmail.com>
2024-01-20 13:25:31 +0100

    removed mv command from deployment script.

--- render_deploy.sh
+++ render_deploy.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "setting up app dependencies..."
 curl -L https://github.com/golang-migrate/migrate/releases/download/v4.16.2/migrate.linux-amd64.tar.gz | tar xvz
-mv ./migrate /usr/bin/migrate
+#mv ./migrate /usr/bin/migrate
 make migrateup
 
 echo "starting app..."
