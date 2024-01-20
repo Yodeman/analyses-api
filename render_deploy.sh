f91ff2ae7e83c402d1446994767493bbc19b2de1
Paul Oyelabi <oyelabipaul@gmail.com>
2024-01-20 13:30:38 +0100

    added logs to deployment script.

--- render_deploy.sh
+++ render_deploy.sh
@@ -5,6 +5,9 @@ set -e
 echo "setting up app dependencies..."
 curl -L https://github.com/golang-migrate/migrate/releases/download/v4.16.2/migrate.linux-amd64.tar.gz | tar xvz
 #mv ./migrate /usr/bin/migrate
+echo $PATH
+export PATH=$PATH:$PWD
+echo $PATH
 make migrateup
 
 echo "starting app..."
